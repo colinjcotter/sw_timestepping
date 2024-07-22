5f12497cc688e116a3c17b5acb2ac270a67fa30a
Colin Cotter <colin.cotter@imperial.ac.uk>
2024-07-22 14:51:17 +0100

    use ssprk3

--- imex.py
+++ imex.py
@@ -100,16 +100,15 @@ while t < tmax + 0.5*dt:
         Un.assign(Unp1)
 
     # a full explicit step using
-    # heun scheme
-    # yhat = y_0 + hf(y_0)
-    # y_1 = y_0 + h/2*(f(y_0) + f(yhat))
-    #     = (y_0 + h*f(y_0))/2 + (y_0 + h*f(yhat))/2)
+    # SSPRK3
     with PETSc.Log.Event("explicit solver"):
         Uhat.assign(Un)
-        expsolver.solve()  #  Unp1 contains Un + h*f(y_0)
-        Uhat.assign(Un + Unp1) #  Uhat contains yhat = Un + h*f(y_0)
-        expsolver.solve()  #  Unp1 contains Un + h*f(yhat)
-        Un.assign((Un + Unp1)/2)
+        expsolver.solve()  #  Unp1 contains U_1 = Un + h*f(Un)
+        Un.assign(Unp1)
+        expsolver.solve()  #  Unp1 contains U_2 = U_1 + h*f(U_1)
+        Un.assign(.75*Uhat + 0.25*Unp1)  # U_2 -> 0.75*Un + 0.25*U_2
+        expsolver.solve()  #  Unp1 contains U_3 = U_2 + h*f(U_2)
+        Un.assign(Uhat/3 + 2*Unp1/3)
 
     # half an implicit step
     with PETSc.Log.Event("implicit solver"):
