e57b23fce75492863a139a666a5f6c85beb0cc2e
Colin Cotter <colin.cotter@imperial.ac.uk>
2024-07-22 15:28:26 +0100

    another bug

--- imex.py
+++ imex.py
@@ -106,7 +106,7 @@ while t < tmax + 0.5*dt:
         expsolver.solve()  #  Unp1 contains U_1 = Un + h*f(Un)
         Uhat.assign(Unp1)
         expsolver.solve()  #  Unp1 contains U_2 = U_1 + h*f(U_1)
-        Uhat.assign(.75*Uhat + 0.25*Unp1)  # U_2 -> 0.75*Un + 0.25*U_2
+        Uhat.assign(.75*Un + 0.25*Unp1)  # U_2 -> 0.75*Un + 0.25*U_2
         expsolver.solve()  #  Unp1 contains U_3 = U_2 + h*f(U_2)
         Un.assign(Un/3 + 2*Unp1/3)
 
