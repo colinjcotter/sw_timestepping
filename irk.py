9c9fdee690cbf303ea1a206cfc5ba94fb79b827b
Colin Cotter <colin.cotter@imperial.ac.uk>
2025-03-10 08:37:59 +0000

    tried to make a semiimplicit pc

--- irk.py
+++ irk.py
@@ -25,137 +25,41 @@ eqn = (
     + h_op(phi, u0, h0)
 )
 
-if args.sdc:
-    parameters = {"mat_type": "matfree",
-                  "ksp_type": "gmres",
-                  "ksp_monitor": None,
-                  "ksp_atol": 1.0e-50,
-                  "ksp_rtol": 1.0e-8,
-                  "pc_type": "python",
-                  "pc_python_type": "__main__.PQPC",
-                  "aux" : 
-                  {"pc_type": "fieldsplit",   # block preconditioner
-                   "pc_fieldsplit_type": "additive"  # block diagonal
-                   },
-                  "snes_monitor": None,
-                  "snes_lag_preconditioner": 5,
-                  }
-
-    per_field={
-        "ksp_type": "preonly",
-        "pc_type": "mg",
-        "pc_mg_cycle_type": "v",
-        "pc_mg_type": "multiplicative",
-        "mg_levels_ksp_type": "gmres",
-        "mg_levels_ksp_max_it": 3,
-        #"mg_levels_ksp_convergence_test": "skip",
-        "mg_levels_pc_type": "python",
-        "mg_levels_pc_python_type": "firedrake.PatchPC",
-        "mg_levels_patch_pc_patch_save_operators": True,
-        "mg_levels_patch_pc_patch_partition_of_unity": True,
-        "mg_levels_patch_pc_patch_sub_mat_type": "seqdense",
-        "mg_levels_patch_pc_patch_construct_dim": 0,
-        "mg_levels_patch_pc_patch_construct_type": "star",
-        "mg_levels_patch_pc_patch_local_type": "additive",
-        "mg_levels_patch_pc_patch_precompute_element_tensors": True,
-        "mg_levels_patch_pc_patch_symmetrise_sweep": False,
-        "mg_levels_patch_sub_ksp_type": "preonly",
-        "mg_levels_patch_sub_pc_type": "lu",
-        "mg_levels_patch_sub_pc_factor_shift_type": "nonzero",
-        "mg_coarse_pc_type": "python",
-        "mg_coarse_pc_python_type": "firedrake.AssembledPC",
-        "mg_coarse_assembled_pc_type": "lu",
-        "mg_coarse_assembled_pc_factor_mat_solver_type": "superlu_dist",
-    }
-    
-    for s in range(args.rk_stages):
-        parameters["aux_pc_fieldsplit_"+str(s)+"_fields"] = \
-            str(2*s)+","+str(2*s+1)
-        parameters["aux_fieldsplit_%s" % (s,)] = per_field
-else:
-    parameters = {
-        "snes_monitor": None,
-        "snes_converged_reason": None,
-        "snes_atol": 1e-50,
-        "snes_stol": 1e-50,
-        "snes_rtol": args.ntol,
-        # "snes_max_it": 1,
-        # "snes_convergence_test": "skip",
-        "snes_lag_jacobian": -2,
-        #"snes_lag_jacobian_persists": None,
-        "ksp_monitor": None,
-        "ksp_converged_rate": None,
-        # "ksp_view": None,
-        "ksp_type": "gmres",
-        "ksp_rtol": 1e-6,
-        "ksp_atol": 1e-50,
-        "ksp_max_it": 60,
-        "pc_type": "ksp",
-        "ksp_ksp_type": "richardson",
-        "ksp_ksp_richardson_scale": 0.99,
-        "ksp_ksp_max_it": 2,
-        "ksp_pc_type": "python",
-        "ksp_pc_python_type": "firedrake.PatchPC",
-        "ksp_patch_pc_patch_save_operators": True,
-        "ksp_patch_pc_patch_partition_of_unity": True,
-        "ksp_patch_pc_patch_sub_mat_type": "seqdense",
-        "ksp_patch_pc_patch_construct_dim": 0,
-        "ksp_patch_pc_patch_construct_type": "star",
-        "ksp_patch_pc_patch_local_type": "additive",
-        "ksp_patch_pc_patch_precompute_element_tensors": True,
-        "ksp_patch_pc_patch_symmetrise_sweep": False,
-        "ksp_patch_sub_ksp_type": "preonly",
-        "ksp_patch_sub_pc_type": "lu",
-        "ksp_patch_sub_pc_factor_shift_type": "nonzero"
-    }
-
-patch = {
-    "pc_type": "python",
-    "pc_python_type": "firedrake.PatchPC",
-    "patch_pc_patch_save_operators": True,
-    "patch_pc_patch_partition_of_unity": True,
-    "patch_pc_patch_sub_mat_type": "seqdense",
-    "patch_pc_patch_construct_dim": 0,
-    "patch_pc_patch_construct_type": "star",
-    "patch_pc_patch_local_type": "additive",
-    "patch_pc_patch_precompute_element_tensors": True,
-    "patch_pc_patch_symmetrise_sweep": False,
-    "patch_sub_ksp_type": "preonly",
-    "patch_sub_pc_type": "ilu",
-    "patch_sub_pc_factor_shift_type": "nonzero"
-}
-
-fields0 = ""
-fields1 = ""
-for i in range(args.rk_stages):
-    fields0 += str(2*i+1)
-    fields1 += str(2*i)
-    if i < args.rk_stages-1:
-        fields0 += ", "
-        fields1 += ", "
-
-schur_parameters = {
+parameters = {
     "snes_monitor": None,
     "snes_converged_reason": None,
     "snes_atol": 1e-50,
     "snes_stol": 1e-50,
     "snes_rtol": args.ntol,
-    #"snes_lag_jacobian": -2,
-    #"snes_lag_jacobian_persists": None,
+    # "snes_max_it": 1,
+    # "snes_convergence_test": "skip",
+    "snes_lag_jacobian": 4,
+    "snes_lag_jacobian_persists": None,
     "ksp_monitor": None,
     "ksp_converged_rate": None,
+    # "ksp_view": None,
     "ksp_type": "gmres",
-    "ksp_rtol": 1e-6,
+    "ksp_rtol": args.ktol,
     "ksp_atol": 1e-50,
     "ksp_max_it": 60,
-    "pc_type": "fieldsplit",
-    "pc_fieldsplit_type": "multiplicative",
-    "fieldsplit_0_fields": fields0,
-    "fieldsplit_1_fields": fields1,
-    "fieldsplit_0" : patch,
-    "fieldsplit_1" : patch
+    "pc_type": "ksp",
+    "ksp_ksp_type": "richardson",
+    "ksp_ksp_richardson_scale": 0.8,
+    "ksp_ksp_max_it": 2,
+    "ksp_pc_type": "python",
+    "ksp_pc_python_type": "firedrake.PatchPC",
+    "ksp_patch_pc_patch_save_operators": True,
+    "ksp_patch_pc_patch_partition_of_unity": True,
+    "ksp_patch_pc_patch_sub_mat_type": "seqdense",
+    "ksp_patch_pc_patch_construct_dim": 0,
+    "ksp_patch_pc_patch_construct_type": "star",
+    "ksp_patch_pc_patch_local_type": "additive",
+    "ksp_patch_pc_patch_precompute_element_tensors": True,
+    "ksp_patch_pc_patch_symmetrise_sweep": False,
+    "ksp_patch_sub_ksp_type": "preonly",
+    "ksp_patch_sub_pc_type": "lu",
+    "ksp_patch_sub_pc_factor_shift_type": "nonzero"
 }
-
 
 stepper = TimeStepper(eqn, butcher_tableau, tc, dT, Un,
                       solver_parameters=parameters)
