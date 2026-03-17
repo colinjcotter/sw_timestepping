7a8e255782d24a22275c1c3f90db552353e3a2d4
Colin Cotter <colin.cotter@imperial.ac.uk>
2026-03-17 08:56:35 +0000

    123

--- run_irks.py
+++ run_irks.py
@@ -1,7 +1,7 @@
 levels = ["6"]
 dts = [14400, 10800, 7200, 3600, 2400, 1200, 600, 300]
 pcs = ["mg"]
-stages = [1,2] # overridden in the presence of DIRKs, best not to mix DIRK and FIRK
+stages = [1,2,3] # overridden in the presence of DIRKs, best not to mix DIRK and FIRK
 tmax = 86400
 ntol = 1.0e-6
 williamson=6
